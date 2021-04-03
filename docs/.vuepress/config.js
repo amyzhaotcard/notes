88729aea0fa1ea0270946f6cbb598fcee0b04157
heyuelan <yuelan_he@163.com>
2021-04-03 23:36:33 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  base: '/notes/',
+  title: '知食',
+  description: '大明湖畔-王翠花',
+  dest: './dist',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+    }
+  },
+  head: [
+    ['link', { rel: 'icon', href: '/logo-2.jpeg' }]
+  ],
+  theme: '',
+  port: 9000,
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'VuePress', link: 'https://vuepress.vuejs.org/zh/' },
+    ],
+    sidebar: [
+      {
+        title: '面试',
+        path: '/mianshi',
+        collapsable: true,
+        sidebarDepth: 1,
+        children: [
+          '/mianshi/JS/数据类型',
+          '/mianshi/JS/作用域和作用域链',
+          '/mianshi/JS/闭包',
+          '/mianshi/JS/原型和原型链',
+          '/mianshi/JS/this的指向',
+          '/mianshi/JS/call-apply-bind',
+          '/mianshi/JS/new操作符',
+          '/mianshi/JS/继承',
+          '/mianshi/JS/深浅拷贝',
+          '/mianshi/JS/Promise',
+          '/mianshi/JS/instanceof',
+          '/mianshi/JS/常用的方法',
+          '/mianshi/JS/数组',
+          '/mianshi/CSS相关/',
+          '/mianshi/算法/'
+        ]
+      },
+    ],
+    lastUpdated: '上次更新'
+  },
+  plugins: [
+    '@vuepress/back-to-top',
+    [
+      'vuepress-plugin-zooming',
+      {
+        selector: '.page img',
+        delay: 1000,
+        options: {
+          bgColor: 'black',
+          zIndex: 10000
+        }
+      }
+    ]
+  ]
+}
